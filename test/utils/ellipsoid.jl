d54f9500ca28dfe24d1b5ec895dedf62778642db
adrienbanse <adrien.banse@gmail.com>
2023-06-30 15:49:25 +0200

    clean ellispoids \in, \notin

--- test/utils/ellipsoid.jl
+++ test/utils/ellipsoid.jl
@@ -9,7 +9,6 @@ const DI = Dionysos
 const UT = DI.Utils
 
 @testset "EllipsoidBasics" begin
-
     c1 = [1.0; 1.0]
 
     P_neg = [
@@ -91,58 +90,58 @@ end
     a = 0.8
     E1 = UT.Ellipsoid(P0, c0 + [a; a])
 
-    @test UT.intersect(E1, E) == true
-    @test UT.intersect(E, E1) == true
+    @test UT.intersect(E1, E)
+    @test UT.intersect(E, E1)
     E1scaled = UT.scale_for_noninclusion_contact_point(E1, E)
     err = norm(E1scaled.c - [2.4; 2.2]) + norm(E1scaled.P - [2.388 -0.597; -0.597 2.985])
     @test err <= 10e-4
-    @test Base.in(E1, E) == false
-    @test Base.in(E, E1) == true
-    @test Base.in(E, E1scaled) == false
-    @test Base.in(E1scaled, E) == false
+    @test E1 ∉ E
+    @test E ∈ E1
+    @test E ∉ E1scaled
+    @test E1scaled ∉ E
     E1scaled2 = UT.scale_for_inclusion_contact_point(E1, E)
     err = norm(E1scaled2.c - [2.4; 2.2]) + norm(E1scaled2.P - [0.466 -0.116; -0.116 0.582])
     @test err <= 10e-2
-    @test Base.in(E, UT.Ellipsoid(E1scaled2.P * 0.99, E1scaled2.c)) == true
+    @test E ∈ UT.Ellipsoid(E1scaled2.P * 0.99, E1scaled2.c)
     ############################################
     a = 1.2
     E1 = UT.Ellipsoid(P0, c0 + [a; a])
 
-    @test UT.intersect(E1, E) == true
-    @test UT.intersect(E, E1) == true
+    @test UT.intersect(E1, E)
+    @test UT.intersect(E, E1)
     E1scaled = UT.scale_for_noninclusion_contact_point(E1, E)
     err = norm(E1scaled.c - [2.8; 2.599]) + norm(E1scaled.P - [0.725 -0.181; -0.181 0.906])
     @test err <= 1e-2
-    @test Base.in(E1, E) == false
-    @test Base.in(E, E1) == false
-    @test Base.in(E, E1scaled) == false
-    @test Base.in(E1scaled, E) == false
+    @test E1 ∉ E
+    @test E ∉ E1
+    @test E ∉ E1scaled
+    @test E1scaled ∉ E
     E1scaled2 = UT.scale_for_inclusion_contact_point(E1, E)
     err =
         norm(E1scaled2.c - [2.8; 2.599]) + norm(E1scaled2.P - [0.254 -0.063; -0.063 0.318])
     @test err <= 10e-2
-    @test Base.in(E, UT.Ellipsoid(E1scaled2.P * 0.99, E1scaled2.c)) == true
+    @test E ∈ UT.Ellipsoid(E1scaled2.P * 0.99, E1scaled2.c)
     ############################################
     a = 2.5
     E1 = UT.Ellipsoid(P0, c0 + [a; a])
 
-    @test UT.intersect(E1, E) == false
-    @test UT.intersect(E, E1) == false
+    @test !UT.intersect(E1, E)
+    @test !UT.intersect(E, E1)
     E1scaled = UT.scale_for_noninclusion_contact_point(E1, E)
     err =
         norm(E1scaled.c - [4.1; 3.9]) +
         norm(E1scaled.P - [0.1195 -0.02989; -0.029897 0.14948])
     @test err <= 10e-4
-    @test Base.in(E1, E) == false
-    @test Base.in(E, E1) == false
-    @test Base.in(E, E1scaled) == false
-    @test Base.in(E1scaled, E) == false
+    @test E1 ∉ E
+    @test E ∉ E1
+    @test E ∉ E1scaled
+    @test E1scaled ∉ E
     E1scaled2 = UT.scale_for_inclusion_contact_point(E1, E)
     err =
         norm(E1scaled2.c - [4.1; 3.9]) +
         norm(E1scaled2.P - [0.073295 -0.018323; -0.01832382 0.0916196])
     @test err <= 10e-4
-    @test Base.in(E, UT.Ellipsoid(E1scaled2.P * 0.99, E1scaled2.c)) == true
+    @test E ∈ UT.Ellipsoid(E1scaled2.P * 0.99, E1scaled2.c)
 
     #############################################
     Id = [
